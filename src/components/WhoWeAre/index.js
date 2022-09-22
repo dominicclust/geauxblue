a5c339acff1fd3466ec7e1482ba88eaae94dcfe7
dominicclust <dclust34@gmail.com>
2022-09-21 19:45:41 -0500

    first attempt

--- src/components/WhoWeAre/index.js
+++ src/components/WhoWeAre/index.js
@@ -9,16 +9,21 @@ const WhoWeAre = () => {
                 Geaux Blue NOLA was first established in 2016 in response to tragic and senseless attacks against
                 police officers in cities across the country including places like Dallas and Baton Rouge. The
                 intent was to show public support and appreciation for NOPD officers while improving morale within
-                the ranks. Both events were repeated in 2017 the last events to take place. In 2022 the need was
+                the ranks. 
+            </p>
+            <p>
+                Both events were repeated in 2017 the last events to take place. In 2022 the need was
                 recognized to once again show the support and appreciation for our hometown heroes, as the number
                 of officers began to dwindle.
-
+            </p>
+            <p>
                 Join us in showing your support for the New Orleans Police Department's officers and their
                 families. We are pleased to announce that plans are well underway for both the third Geaux Blue
                 Family Day and Geaux Blue NOLA Day. Past Geaux Blue NOLA events were widely adopted and supported
                 by the public and the business community in past years. They were an amazing success and we hope
                 to build on that momentum this year.
-
+            </p>
+            <p>
                 NOPD officers are dedicated to providing our residents and visitors with the best public safety
                 possible, sometimes under the most adverse and demanding circumstances. In addition, for every
                 officer, there are family members that sacrifice holidays, nights, weekends, and family events so
